248d20769efed5bfac52ba45b3d11017e7efa084
Abigail <abigail@abigail.freedom.nl>
2026-05-06 00:29:48 +0200

    Ruby and Tcl solutions for week 372/1

---
+++ challenge-372/abigail/tcl/ch-1.tcl
@@ -0,0 +1,43 @@
+#!/opt/homebrew/bin/tclsh
+
+while {[gets stdin line] >= 0} {
+    #
+    # Remove quotes
+    #
+    regsub -all {"} $line {} line
+
+    #
+    # Get the number of spaces; we do this by comparing the length of
+    # the string, with the length of the string with the spaces removed
+    #
+    set spaces [expr [string length $line] - \
+                     [string length [regsub -all { } $line {}]]]
+
+    #
+    # Number of gaps between the words
+    #
+    set gaps [expr [llength $line] - 1]
+
+    #
+    # Calculate the number of spaces needed
+    #
+    set per_gap 0
+    if {$gaps > 0} {
+        set per_gap [expr $spaces / $gaps]
+    }
+    set end [expr $spaces - $gaps * $per_gap]
+
+    #
+    # Print the result
+    #
+    puts -nonewline {"}
+    for {set index 0} {$index < [llength $line]} {incr index} {
+        puts -nonewline [lindex $line $index]
+        set times $per_gap
+        if {$index == [llength $line] - 1} {
+            set times $end
+        }
+        puts -nonewline [string repeat { } $times]
+    }
+    puts {"}
+}
