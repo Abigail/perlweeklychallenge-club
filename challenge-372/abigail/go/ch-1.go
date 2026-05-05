c270858512ce1ad31daa30466dd7598c1653f8d8
Abigail <abigail@abigail.freedom.nl>
2026-05-06 01:03:22 +0200

    Improve Go solution for week 372/2
    Instead of printing spaces one by one, make use of Join and Repeat

--- challenge-372/abigail/go/ch-1.go
+++ challenge-372/abigail/go/ch-1.go
@@ -4,7 +4,7 @@ import (
     "fmt"
     "bufio"
     "os"
-    "strings"
+    S "strings"
 )
 
 func main () {
@@ -15,29 +15,21 @@ func main () {
             break
         }
         
-        text     = strings . ReplaceAll (text, "\"", "")  // Remove quotes
-        text     = strings . ReplaceAll (text, "\n", "")  // Remove newline
-        spaces  := strings . Count      (text, " ")       // Count spaces
-        words   := strings . Fields     (text)            // Split into words
-        gaps    := len (words) - 1                        // Nr of gaps
+        text     = S . ReplaceAll (text, "\"", "")  // Remove quotes
+        text     = S . ReplaceAll (text, "\n", "")  // Remove newline
+        spaces  := S . Count      (text, " ")       // Count spaces
+        words   := S . Fields     (text)            // Split into words
 
-        per_gap := 0                                      // Calculate amount
-        if gaps > 0 {                                     //   of spaces needed
+        gaps    := len (words) - 1                  // Nr of gaps
+
+        per_gap := 0                                // Calculate amount
+        if gaps > 0 {                               //   of spaces needed
             per_gap = spaces / gaps
         }
         end     := spaces - gaps * per_gap
 
-        fmt . Printf ("\"")                               // Print quote
-        for index, value := range (words) {
-            fmt . Printf ("%s", value)                    // Print word
-            max := end
-            if index < len (words) - 1 {
-                max = per_gap
-            }
-            for i := 0; i < max; i ++ {                   // Print gap,
-                fmt . Printf (" ")                        //   space by space
-            }
-        }
-        fmt . Printf ("\"\n")                             // Print quote
+        fmt . Println ("\"" + S . Join (words, S . Repeat (" ", per_gap)) +
+                                               S . Repeat (" ", end)      +
+                       "\"")
     }
 }
